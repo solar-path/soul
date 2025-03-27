ab8fc58adcc6773457fe2ed6fa3fc725ea5fba46
solar-path <55182918+solar-path@users.noreply.github.com>
2025-03-27 19:28:53 +0500

    install tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
     react(),
-    // ...,
+    tailwindcss(),
   ],
 });
