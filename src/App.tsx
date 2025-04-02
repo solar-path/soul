1fe5054d1b54fbc09a9ed9329f43959ecad19b8c
solar-path <55182918+solar-path@users.noreply.github.com>
2025-04-02 16:45:04 +0500

    add setup tanstack query

--- src/App.tsx
+++
@@ -1,5 +0,0 @@
-function App() {
-  return <>works</>;
-}
-
-export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,8 @@ import { RouterProvider, createRouter } from "@tanstack/react-router";
 import "./global.css";
 // Import the generated route tree
 import { routeTree } from "./routeTree.gen";
-
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./utils/trpc";
 // Create a new router instance
 const router = createRouter({ routeTree });
 
@@ -21,7 +22,9 @@ if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
     <StrictMode>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </StrictMode>
   );
 }
